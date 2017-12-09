5d8f441a4d48329139993442d07dcb113dcaed1d
Elise <elise@dettrick.net>
2017-12-09 05:11:00 +0000

    challange 2: complete

--- server.js
+++ server.js
@@ -52,7 +52,36 @@ app.get('/get-posts', function(request, response) {
     });
 });
 
-
+app.delete('/delete-post/:timestamp', function(request, response) {
+    var ts = request.params.timestamp;
+    
+    fs.readFile(__dirname+'/data/posts.json', function(error, data) {
+        if(error) {
+            console.log('Error reading posts.json: ' + error);
+            response.status(500);
+            response.send(error);
+        } else {
+            var posts = JSON.parse(data);
+            
+            var remainingPosts = posts.blogposts.filter(function(p) {
+                return p.timestamp != ts;
+            });
+            
+            posts.blogposts = remainingPosts;
+            var updatedData = JSON.stringify(posts);
+            
+            fs.writeFile(__dirname + '/data/posts.json', updatedData, function(error) {
+                if(error) {
+                    console.log('Error reading posts.json: ' + error);
+                    response.status(500);
+                    response.send({ "success": false });
+                } else {
+                    response.send({ "success": true });
+                }
+            });
+        }
+    });
+});
 
 app.listen(8080, function() {
     console.log('The server has started and is listening on port 8080');
